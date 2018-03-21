3c2191f6093d3195f78b9dd30c3d0aaefee743a9
Pavel Bernshtam <pavelb@cgen.com>
2018-03-21 16:05:30 +0200

    make it working on tomcat on linux

--- src/main/kotlin/org/fbgroups/config/Application.kt
+++ src/main/kotlin/org/fbgroups/config/Application.kt
@@ -3,11 +3,15 @@ package org.fbgroups.config
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.web.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.PropertySource
 import org.springframework.context.annotation.PropertySources
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.boot.builder.SpringApplicationBuilder
+
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = ["org.fbgroups"])
@@ -15,7 +19,11 @@ import org.springframework.scheduling.annotation.EnableAsync
 @EnableJpaRepositories(basePackages = ["org.fbgroups.entity"])
 @EnableAsync
 @PropertySources(PropertySource("classpath:application.properties"), PropertySource("file:\${user.home}/properties/fbgroup.properties"))
-class Application
+class Application :SpringBootServletInitializer() {
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(Application::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
